85922c091a835234ee814648666e7a832fb3e2a9
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-07-20 16:05:30 +0630

    fixed registration bug

--- src/main/java/com/ace/hexa/controller/AdminController.java
+++ src/main/java/com/ace/hexa/controller/AdminController.java
@@ -123,7 +123,6 @@ public class AdminController {
 				model.addAttribute("err", "Delete Failed!");
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
